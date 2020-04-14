1ee136023c4b20b99b7e19f6b01fd9810ae1931a
Park Soo Jin <soojinsarah@naver.com>
2020-04-14 17:00:07 +0900

    adding sortPrice function low-to-high

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,51 @@
     <img alt="Vue logo" src="./assets/logo.png">
 
     <h4>Hello world, this is {{name}}.</h4>
+    <p v-bind:style="fonts">font size</p>
 
-    <Card/>
+    <button v-on:click="sortPrice">가격순으로 정렬하기</button>
+
+    <p>{{Oneroom[2].title}}</p>
+    <img v-bind:src="Oneroom[2].image" alt="">
+    <p>{{Oneroom[2].caption}}</p>
+    <p>{{Oneroom[2].price}}</p>
+
+    <p>{{Oneroom[1].title}}</p>
+    <img v-bind:src="Oneroom[1].image" alt="">
+    <p>{{Oneroom[1].caption}}</p>
+    <p>{{Oneroom[1].price}}</p>
+
+    <p>{{Oneroom[0].title}}</p>
+    <img v-bind:src="Oneroom[0].image" alt="">
+    <p>{{Oneroom[0].caption}}</p>
+    <p>{{Oneroom[0].price}}</p>
+
 
   </div>
 </template>
 
 <script>
-import Card from './components/Card.vue';
 import Oneroom from './assets/rooms_post.js';
 
 export default {
   name: 'App',
   components: {
-    Card,
+
   },
   data() {
     return {
       name: "sarah",
       Oneroom: Oneroom,
+      fonts: "font-size:26px",
     };
   },
+  methods:{
+    sortPrice(){
+      this.Oneroom.sort(function(a,b){
+        return a.price-b.price
+      });
+    },
+  }
 }
 </script>
 
