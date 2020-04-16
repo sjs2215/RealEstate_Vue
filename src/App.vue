e04c7ee3f8ece5430eaae3541b25a3391242d868
Park Soo Jin <soojinsarah@naver.com>
2020-04-16 14:44:09 +0900

    practicing v-for in li tag

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,22 @@
     <img alt="Vue logo" src="./assets/logo.png">
 
     <h4 v-if="flag">Hello world, this is {{name}}.</h4>
-    <p v-bind:style="fonts">font size</p>
 
-    <button v-on:click="sortPrice">가격순으로 정렬하기</button>
     <button v-on:click="hideName"> 이름 숨기기</button>
 
-    <p>{{Oneroom[2].title}}</p>
-    <img v-bind:src="Oneroom[2].image" alt="">
-    <p>{{Oneroom[2].caption}}</p>
-    <p>{{Oneroom[2].price}}</p>
-
-    <p>{{Oneroom[1].title}}</p>
-    <img v-bind:src="Oneroom[1].image" alt="">
-    <p>{{Oneroom[1].caption}}</p>
-    <p>{{Oneroom[1].price}}</p>
-
-    <p>{{Oneroom[0].title}}</p>
-    <img v-bind:src="Oneroom[0].image" alt="">
-    <p>{{Oneroom[0].caption}}</p>
-    <p>{{Oneroom[0].price}}</p>
-
+    <Card/>
 
   </div>
 </template>
 
 <script>
 import Oneroom from './assets/rooms_post.js';
+import Card from './components/Card.vue';
 
 export default {
   name: 'App',
   components: {
-
+    Card: Card,
   },
   data() {
     return {
@@ -41,6 +26,7 @@ export default {
       Oneroom: Oneroom,
       fonts: "font-size:26px",
       flag: "true",
+      food: ['chicken', 'pizza', 'ramen', 'dumplings'],
     };
   },
   methods:{
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,24 +1,18 @@
 <template>
-  <div class="container">
-      <div class="row">
-        <div class="col-md-2">메뉴</div>
-        <div class="col-md-10">
-          <div class="row">
-            <div class="col-md-6">
-              <img src="">
-              <h4>상품명</h4>
-              <p>가격</p>
-            </div>
-            <div class="col-md-6">상품2..</div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <ul>
+      <li v-for="이름들 in food" v-bind:key="이름들">{{이름들}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
 
+    };
+  }
 }
 </script>
 
